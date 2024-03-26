96c261a40c6277bb8adf36a548c0b25ada33bb09
hakertzLabMn <hakertzlabmn@gmail.com>
2024-03-26 19:53:53 +0100

    Update test files
    Signed-off-by: Samuele Toscani <271968@studenti.unimore.it>

--- src/methods/selfsupervised/simclr/test/test.py
+++ src/methods/selfsupervised/simclr/test/test.py
@@ -9,8 +9,7 @@ from wilds.common.data_loaders import get_eval_loader
 from src.methods.selfsupervised.simclr.utils.tsne_visualization import TSNEVisualization
 
 from src.methods.classifier.model.features_extractor import FeaturesExtractor
-from src.methods.selfsupervised.simclr.model.projection_head import ProjectionHead
-from src.methods.selfsupervised.simclr.model.simclr import SimCLR
+from src.utils.dataaugmentation.self_standardization import SelfStandardization
 
 
 class Test:
@@ -19,13 +18,11 @@ class Test:
 
         self.device = None
         self.config = None
-        self.data_augmentation = None
+        self.transform = None
         self.eval_dataset = None
         self.eval_dataloader = None
-        self.model = None
-
-        self.labels = []
-        self.embedding = []
+        self.features_extractor = None
+        self.tsne_visualization = None
 
         self.initialize()
 
@@ -35,6 +32,8 @@ class Test:
         self.define_dataset()
         self.define_dataloader()
         self.define_model()
+
+        self.tsne_visualization = TSNEVisualization(self.config["hyper_parameters"]["experiments"])
 
     def define_config(self):
         with open(self.config_path, "r") as stream:
@@ -46,8 +45,9 @@ class Test:
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def define_transform(self):
-        self.data_augmentation = Compose([
-            ToTensor()
+        self.transform = Compose([
+            ToTensor(),
+            SelfStandardization()
         ])
 
     def define_dataset(self):
@@ -56,8 +56,9 @@ class Test:
             download=self.config["dataset"]["download"]
         )
         self.eval_dataset = dataset.get_subset(
-            split="val",
-            transform=self.data_augmentation,
+            split=self.config["dataset"]["split"],
+            frac=self.config["dataset"]["fraction"],
+            transform=self.transform,
         )
 
     def define_dataloader(self):
@@ -69,39 +70,26 @@ class Test:
         )
 
     def define_model(self):
-        features_extractor = FeaturesExtractor(
+        self.features_extractor = FeaturesExtractor(
             latent_dim=self.config["model"]["features_extractor"]["latent_dim"],
             weights=self.config["model"]["features_extractor"]["weights"]
         )
-        projection_head = ProjectionHead(
-            latent_dim=self.config["model"]["project_head"]["latent_dim"],
-            projection_dim=self.config["model"]["project_head"]["projection_dim"],
-        )
-        self.model = SimCLR(features_extractor, projection_head)
-        self.model.to(self.device)
-        self.model.eval()
+        self.features_extractor.to(self.device)
+        self.features_extractor.eval()
 
     def test(self):
-        cell_lines = ["HEPG2", "HUVEC", "RPE", "U2OS"]
-        '''expriments = [
-            "HEPG2-01", "HEPG2-02", "HEPG2-03", "HEPG2-04","HEPG2-05", "HEPG2-06", "HEPG2-07",
-            "HEPG2-08", "HEPG2-09", "HEPG2-10", "HEPG2-11",
-            "HUVEC-01", "HUVEC-02", "HUVEC-03", "HUVEC-04", "HUVEC-05", "HUVEC-06", "HUVEC-07",
-            "HUVEC-08", "HUVEC-09", "HUVEC-10", "HUVEC-11", "HUVEC-12", "HUVEC-13", "HUVEC-14",
-            "HUVEC-15", "HUVEC-16", "HUVEC-17", "HUVEC-18", "HUVEC-19", "HUVEC-20", "HUVEC-21",
-            "HUVEC-22", "HUVEC-23", "HUVEC-24",
-            "RPE-01", "RPE-02", "RPE-03", "RPE-04", "RPE-05", "RPE-06", "RPE-07", "RPE-08", "RPE-09", "RPE-10", "RPE-11",
-            "U2OS-01", "U2OS-02", "U2OS-03", "U2OS-04", "U2OS-05"]'''
-        tsne_visualization = TSNEVisualization(cell_lines)
         with torch.no_grad():
             for iteration, labeled_batch in enumerate(zip(self.eval_dataloader)):
                 images, y, metadata = labeled_batch[0]
-                image = images.to(self.device)
-                embedding = self.model.encode(image)
-                embedding = embedding.cpu().data
-                labels = metadata[:, 0] #metadata[:, 1]
-                self.labels.append(labels)
-                self.embedding.append(embedding)
-        self.labels = torch.cat(self.labels, dim=0)
-        self.embedding = torch.cat(self.embedding, dim=0)
-        tsne_visualization.visualize(self.embedding, self.labels)
+                images = images.to(self.device).to(torch.float32)
+                labels = metadata[:, 1].to(self.device)
+
+                # mask = torch.logical_and(metadata[:, 1] >= 11, metadata[:, 1] <= 26)
+                # images, y, metadata, labels = images[mask], y[mask], metadata[mask], labels[mask]
+
+                embeddings = self.features_extractor(images)
+
+                self.tsne_visualization.update(embeddings, labels)
+
+        self.tsne_visualization.compute()
+        self.tsne_visualization.plot_embeddings()
--- src/methods/selfsupervised/simclr/utils/tsne_visualization.py
+++ src/methods/selfsupervised/simclr/utils/tsne_visualization.py
@@ -1,29 +1,45 @@
-from sklearn.manifold import TSNE
+import torch
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.manifold import TSNE
+
 
 class TSNEVisualization:
-    def __init__(self, clases):
-        self.classes = clases
-        self.tsne = TSNE()
+    def __init__(self, classes):
+        self.classes = classes
 
-    def visualize(self, embedding, labels):
-        embedding_tsne = self.tsne.fit_transform(embedding)
-        labels = [self.classes[l] for l in labels]
-        self.plot_vecs_n_labels(embedding_tsne, labels)
+        self.labels = []
+        self.embeddings = []
 
-    def plot_vecs_n_labels(self, v, labels):
+    def initialize(self):
+        pass
+
+    def update(self, embeddings, labels):
+        self.embeddings.append(embeddings)
+        self.labels.append(labels)
+
+    def compute(self):
+        self.embeddings = torch.cat(self.embeddings, dim=0).cpu()
+        self.labels = torch.cat(self.labels, dim=0).cpu()
+        # print(len(torch.unique(self.labels)), torch.unique(self.labels).tolist())
+
+        tsne = TSNE()
+        self.embeddings = tsne.fit_transform(self.embeddings)
+        self.labels = [self.classes[l] for l in self.labels]
+
+    def plot_embeddings(self):
         fig = plt.figure(figsize=(10, 10))
         plt.axis("off")
         sns.set_style("darkgrid")
-        plt.legend(self.classes)
+        plt.legend(self.labels)
         sns.scatterplot(
-            x=v[:, 0], y=v[:, 1],
-            hue=labels,
-            hue_order=self.classes,
+            x=self.embeddings[:, 0], y=self.embeddings[:, 1],
+            hue=self.labels,
+            hue_order=self.labels,
             legend="full",
-            palette=sns.color_palette("bright", n_colors=len(self.classes)) # rocket
+            palette=sns.color_palette("icefire", n_colors=len(self.labels))
         )
+        # plt.savefig("src/results/tmp/embedding_default.png")
         plt.show()
